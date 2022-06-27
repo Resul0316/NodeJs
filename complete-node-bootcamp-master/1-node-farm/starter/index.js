1d2e893a4407b336bd360b268ddbb478762bd0ce
resul <resulavsar003@gmail.com>
2022-06-27 07:48:34 +0200

    Building a simple API

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const http = require('http');
+const path = require('path');
+const url = require('url');
 
 //* * * * * * * * FIles * * * * * * * * *//
 
@@ -36,11 +38,30 @@ const http = require('http');
 
 
 //* * * * * * * * Server * * * * * * * * *//
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data)
+
 
 const server = http.createServer((req,res) =>{
-    res.end('Hello from the server!');
+    const pathName = req.url;;
+    if ( pathName === '/' || pathName === '/overview'){
+        res.end('This is the OVERVIEW');
+    }else if (pathName === '/product'){
+        res.end('This is the PRODUCT');
+    }else if (pathName === '/api'){
+        
+        res.writeHead(200, {'Content-type' : 'application/json'})
+        res.end(data);
+        
+    }
+     else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'Hello World'
+        })
+        res.end('<h1> Page not found</h1>')
+    }
 });
-
 server.listen(8000, '127.0.0.1', ()=>{
     console.log('Listening the request on port 8000')
-})
+});
