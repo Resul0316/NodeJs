922f43c5c16949ba0545a87ac3d3ce45ac45fb87
Resul0316 <93797905+Resul0316@users.noreply.github.com>
2022-06-29 18:12:33 +0200

    Update index.js

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -37,20 +37,50 @@ const url = require('url');
 
 
 //* * * * * * * * Server * * * * * * * * *//
+const replaceTemplate = (temp, product) =>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    output = output.replace(/{%ID%}/g, product.id)
+
+    if(!product.organic) output = output.replace(/{NOT_ORGANIC}/g, 'not-organic');
+    return output
+
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/template/template-overview`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/template/template-card`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/template/template-product`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data)
 
 const server = http.createServer((req,res) =>{
-    const pathName = req.url;;
+    const pathName = req.url;
+
+    //* Overtview Page * * * *
     if ( pathName === '/' || pathName === '/overview'){
-        res.end('This is the OVERVIEW');
+        res.writeHead(200, {'Content-Type': 'text/html'})
+
+        const cardsHTML = dataObj.map( el =>  replaceTemplate(template, el))
+
+        res.end(tempOverview);
+
+        //* Product Page * * * *
     }else if (pathName === '/product'){
         res.end('This is the PRODUCT');
+
+        //*API
     }else if (pathName === '/api'){
         
         res.writeHead(200, {'Content-type' : 'application/json'})
         res.end(data);
     }
+    //*Not Found
      else {
         res.writeHead(404, {
             'Content-type': 'text/html',
