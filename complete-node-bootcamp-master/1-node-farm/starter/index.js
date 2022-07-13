cf4aa6244bb3a58fbf0b0426148cf92805ebfd15
Resul0316 <93797905+Resul0316@users.noreply.github.com>
2022-07-13 08:09:57 +0200

    Update index.js

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -3,15 +3,7 @@ const http = require('http');
 const path = require('path');
 const url = require('url');
 
-//* * * * * * * * FIles * * * * * * * * *//
-
-
-
-
-
-
-
-
+//* * * * * * * * FIles * * * * * * * * */
 
 
 
